bd6fd95a35da2f50e4d1d0a955e28cbea5f4182b
Andrew Hennessee <ahennes3@student11.cse.nd.edu>
2023-05-02 00:15:06 -0400

    minor formatting

--- Project06/disk.h
+++ Project06/disk.h
@@ -1,4 +1,3 @@
-
 /*
 Do not modify this file.
 Make all of your changes to main.c instead.
@@ -21,7 +20,6 @@ Write exactly BLOCK_SIZE bytes to a given block on the virtual disk.
 "d" must be a pointer to a virtual disk, "block" is the block number,
 and "data" is a pointer to the data to write.
 */
-
 void disk_write( struct disk *d, int block, const unsigned char *data );
 
 /*
@@ -29,13 +27,11 @@ Read exactly BLOCK_SIZE bytes from a given block on the virtual disk.
 "d" must be a pointer to a virtual disk, "block" is the block number,
 and "data" is a pointer to where the data will be placed.
 */
-
 void disk_read( struct disk *d, int block, unsigned char *data );
 
 /*
 Return the number of blocks in the virtual disk.
 */
-
 int disk_nblocks( struct disk *d );
 
 /* 
@@ -46,7 +42,6 @@ int disk_size();
 /*
 Close the virtual disk.
 */
-
 void disk_close( struct disk *d );
 
 #endif
